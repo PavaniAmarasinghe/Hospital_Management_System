64ad61902f74a788b566888945f15a2c36525193
PavaniAmarasinghe <pavaniyureka06@gmail.com>
2024-09-06 23:31:51 +0530

    modify dashboard

--- Dashboard/src/components/Dashboard.jsx
+++ Dashboard/src/components/Dashboard.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../main";
+import { Navigate } from "react-router-dom";
+import axios from "axios";
 
 const Dashboard = () =>{
+    const { isAuthenticated, setIsAuthenticated } = useContext(Context);
+    
+    const [appointments,setAppointments] = useState([]);
 
+    useEffect (()=>{
+        const fetchAppointments = async()=>{
+           try {
+            const {data} = await axios.get("http://localhost:4000/api/v1/appointment/getall",{withCredentials:true});
+            setAppointments(data.appointments)
+        } 
+           catch (error) {
+            setAppointments({});
+            console.log("Some Erooe Occured While Fetching Apoinments",error);
+           } 
+        };
+        fetchAppointments();
+    },[]);
+    
+    
+    if(!isAuthenticated){
+        return<Navigate to = {"/login"}/>;
+    }
    
 
     return(
--- Dashboard/src/components/Doctors.jsx
+++ Dashboard/src/components/Doctors.jsx
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../main";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const Doctors = () =>{
-    return(
-        <div>
+    const [doctors,setDoctors] = useState([]);
+    const {isAuthenticated} = useContext(Context); 
 
-        </div>
-    )
-}
-export default Doctors;
+    useEffect(()=>{
+        const fetchDoctors = async()=>{
+            try {
+                const {data} = await axios.get("http://localhost:4000/api/v1/user/doctors",{withCredentials:true});
+                setDoctors(data.Doctors);
+            } catch (error) {
+              toast.error(error.response.data.message);
+            }
+          };
+        fetchDoctors();
+    },[]);
+
+    return (
+        <section className="page doctors">
+          <h1>DOCTORS</h1>
+          <div className="banner">
+            {doctors && doctors.length > 0 ? (
+              doctors.map((element) => {
+                return (
+                  <div className="card">
+                    <img
+                      src={element.docAvatar && element.docAvatar.url}
+                      alt="doctor avatar"
+                    />
+                    <h4>{`${element.firstName} ${element.lastName}`}</h4>
+                    <div className="details">
+                      <p>Email: <span>{element.email}</span></p>
+                      <p>Phone: <span>{element.phone}</span></p>
+                      <p>DOB: <span>{element.dob.substring(0, 10)}</span></p>
+                      <p>Department: <span>{element.doctorDepartment}</span></p>
+                      <p>NIC: <span>{element.nic}</span></p>
+                      <p>Gender: <span>{element.gender}</span></p>
+                    </div>
+                  </div>
+                );
+              })
+            ) : (
+              <h1>No Registered Doctors Found!</h1>
+            )}
+          </div>
+        </section>
+      );
+    };
+    
+    export default Doctors;
+    
